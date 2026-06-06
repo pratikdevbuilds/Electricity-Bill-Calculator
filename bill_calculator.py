cd1315c004490644e80efb4847dab95e2ef7fb9c
Pratik Patidar <pratikpatidar1992006@gmail.com>
2026-06-06 12:00:03 +0530

    Added Electricity Bill Calculator project

---
+++ bill_calculator.py
@@ -0,0 +1,19 @@
+amount = int(input("Enter amount: "))
+
+balance 
+if  amount >0:
+  
+  balance=balance+amount
+  print("added balance:",balance)
+
+else:
+  if balance<0:
+   print(" balance can not Debited according to  your requirement \n Fristly you need to credit balance\n Check balance right now\n Thank You")
+  else:
+    balance-=amount
+    print("total balance :",balance)
+  
+
+print("total balance :",balance)
+
+  
